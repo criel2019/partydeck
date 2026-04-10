4a5d94629d079489ecdf3785d579cebe7f438684
Criel <pjhk579700@naver.com>
2026-04-11 01:42:57 +0900

    Add COOP/COEP unsafe-none headers to fix popup OAuth flow

--- server/src/middleware/cors.js
+++ server/src/middleware/cors.js
@@ -4,6 +4,8 @@ function cors(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
